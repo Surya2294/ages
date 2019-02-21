56d35f39dc11ff0e25b7cb4ffe77ca83c987aa59
Your Name <surya.pandu315@gmail.com>
2019-02-21 11:52:46 +0530

    initial_1

---
+++ age.py
@@ -0,0 +1,8 @@
+#!/usr/bim/env python3.6
+
+name = input("What is your name?")
+birthdate = input("what is your birthdate ?")
+print(f"(yor birth date is {birthdate} ")
+age = int(input("how old are you?"))
+print(f"{name} was born on {birthdate}")
+print(f"Half of your age is {age / 2}")
